08bc49714027b89d28a202a4a6ed64969afb84ec
Patrick Robin <patrick-robin@gmx.net>
2019-07-08 11:54:28 +0200

    some changes in AppointmentModel.cs

---
+++ DogginatorLibrary/Models/AppointmentModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DogginatorLibrary.Models
+{
+    public class AppointmentModel
+    {
+
+        #region Fields
+
+        #endregion
+
+        #region Properties
+
+        #endregion
+
+        #region Constructor
+
+        #endregion
+
+        #region Methods
+
+        #endregion
+    }
+}
