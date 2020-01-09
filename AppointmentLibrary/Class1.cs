66318c6611359dcdcf4ec80fd4205348ccd28b6c
Patrick Robin <patrick-robin@gmx.net>
2020-01-09 21:02:43 +0100

    Deleted Tests

---
+++ AppointmentLibrary/Class1.cs
@@ -0,0 +1,35 @@
+﻿namespace de.rietrob.dogginator_product.AppointmentLibrary
+
+/*
+*----------------------------------------------------------------------------------
+*          Filename:	Class1.cs
+*          Date:        2020.01.08 20:46:38
+*          All rights reserved
+*
+*----------------------------------------------------------------------------------
+* @author Patrick Robin <support@rietrob.de>
+*/
+
+{
+    public class Class1
+    {
+
+        #region Fields
+
+        
+
+        #endregion
+
+        #region Properties
+
+        #endregion
+
+        #region Constructor
+
+        #endregion
+
+        #region Methods
+
+        #endregion
+    }
+}
