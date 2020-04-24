ca45d4e6e0116e6ba23c47b19a3bb059427d05ee
Patrick Robin <patrick-robin@gmx.net>
2020-04-24 17:39:08 +0200

    Deleted the Test Projects

--- InvoiceLibraryTests/Properties/SharedAssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-using System.Reflection;
- 
-[assembly: AssemblyCompany("RietRob.de")]
-[assembly: AssemblyProduct("Dogginator")]
-[assembly: AssemblyCopyright("Copyright © RietRob.de 2020")]
-[assembly: AssemblyTrademark("Dogginator")]
-[assembly: AssemblyVersion("2020.1.0.1000")]
-[assembly: AssemblyInformationalVersion("2020.1.0")]
